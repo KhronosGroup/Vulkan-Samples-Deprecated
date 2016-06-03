59439e9f03fb10ce668460f1ed04649d3c4583bc
Johannes van Waveren <janpaul.vanwaveren@oculus.com>
2016-06-03 14:51:42 -0500

    atw_vulkan: fixed linux build, pass projection matrix to atw, retain ReducedPrecision in SPIR-V

--- samples/apps/atw/projects/android/ant/atw_cpu_dsp/jni/Android.mk
+++ samples/apps/atw/projects/android/ant/atw_cpu_dsp/jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE			:= atw_cpu_dsp
 LOCAL_SRC_FILES			:= ../../../../../atw_cpu_dsp.c
-LOCAL_CFLAGS			:= -std=c99 -O3 -mfpu=neon -Wall
+LOCAL_CFLAGS			:= -std=c99 -O3 -mfpu=neon -Wall -Wno-unused-function
 LOCAL_LDLIBS			:= -L$(SYSROOT)/usr/lib -llog
 
 include $(BUILD_EXECUTABLE)
