422c332f97270a762de6cab13082b60c409e38e3
Johannes van Waveren <janpaul.vanwaveren@oculus.com>
2016-12-15 18:44:41 -0600

    updated .travis_python.sh

--- .travis_python.sh
+++ .travis_python.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    sw_vers
+    brew update || brew update
 
-    # Install some custom requirements on OS X
-    # e.g. brew install pyenv-virtualenv
+    brew outdated openssl || brew upgrade openssl
+
+    # install pyenv
+    git clone --depth 1 https://github.com/yyuu/pyenv.git ~/.pyenv
+    PYENV_ROOT="$HOME/.pyenv"
+    PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
 
     case "${TOXENV}" in
-        py32)
-            # Install some custom Python 3.2 requirements on OS X
-            ;;
         py33)
-            # Install some custom Python 3.3 requirements on OS X
+            pyenv install 3.3.6
+            pyenv global 3.3.6
             ;;
         py34)
-            # Install some custom Python 3.3 requirements on OS X
+            pyenv install 3.4.5
+            pyenv global 3.4.5
             ;;
         py35)
-            # Install some custom Python 3.3 requirements on OS X
+            pyenv install 3.5.2
+            pyenv global 3.5.2
             ;;
     esac
+    pyenv rehash
+    python -m pip install --user virtualenv
 else
     # Install some custom requirements on Linux
 fi
