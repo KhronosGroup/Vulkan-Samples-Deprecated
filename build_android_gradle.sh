ec6069f6f577b3f05bc6460b8857625e56e37048
gkennickell <gloria.kennickell@oculus.com>
2017-05-24 10:38:18 -0500

    atw_cpu_dsp gradle build include path fix

--- build_android_gradle.sh
+++ build_android_gradle.sh
@@ -4,7 +4,6 @@ mkdir -p ../Vulkan-LoaderAndValidationLayers/build/generated/include
 python3 ../Vulkan-LoaderAndValidationLayers/scripts/lvl_genvk.py -registry ../Vulkan-LoaderAndValidationLayers/scripts/vk.xml vk_dispatch_table_helper.h -o ../Vulkan-LoaderAndValidationLayers/build/generated/include/
 python3 ../Vulkan-LoaderAndValidationLayers/scripts/lvl_genvk.py -registry ../Vulkan-LoaderAndValidationLayers/scripts/vk.xml vk_layer_dispatch_table.h -o ../Vulkan-LoaderAndValidationLayers/build/generated/include/
 
-
 samples/apps/atw/projects/android/gradle/atw_cpu_dsp/gradlew -b samples/apps/atw/projects/android/gradle/atw_cpu_dsp/build.gradle --project-cache-dir build/android/gradle/apps/atw_cpu_dsp/.gradle build
 samples/apps/atw/projects/android/gradle/atw_opengl/gradlew -b samples/apps/atw/projects/android/gradle/atw_opengl/build.gradle --project-cache-dir build/android/gradle/apps/atw_opengl/.gradle build
 samples/apps/atw/projects/android/gradle/atw_vulkan/gradlew -b samples/apps/atw/projects/android/gradle/atw_vulkan/build.gradle --project-cache-dir build/android/gradle/apps/atw_vulkan/.gradle build
--- samples/apps/atw/projects/android/gradle/atw_cpu_dsp/jni/Android.mk
+++ samples/apps/atw/projects/android/gradle/atw_cpu_dsp/jni/Android.mk
@@ -6,7 +6,7 @@ BASE_DIR := ../../../../../../../..
 include $(CLEAR_VARS)
 
 LOCAL_MODULE			:= atw_cpu_dsp
-LOCAL_C_INCLUDES		:= $(BASE_DIR)/external/include
+LOCAL_C_INCLUDES		:= $(BASE_DIR)/Vulkan-Samples/external/include
 LOCAL_SRC_FILES			:= ../../../../../atw_cpu_dsp.c
 LOCAL_CFLAGS			:= -std=c99 -O3 -mfpu=neon -Wall -Wno-unused-function
 LOCAL_LDLIBS			:= -L$(SYSROOT)/usr/lib -llog
